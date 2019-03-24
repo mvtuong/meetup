3007c09029f751c9799eacaf1281570c1c2acb6b
mvtuong <mvtuong90@gmail.com>
2019-03-24 14:52:00 +0100

    Updates

---
+++ precache-manifest.850bc25a2733db2fd9afd180605bcf00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8e7b45e767f26cca2bb",
+    "url": "/meetup/static/css/main.cd7e2ca1.chunk.css"
+  },
+  {
+    "revision": "b8e7b45e767f26cca2bb",
+    "url": "/meetup/static/js/main.6ba0ce60.chunk.js"
+  },
+  {
+    "revision": "448b25596e3b06413538",
+    "url": "/meetup/static/js/runtime~main.34b77dee.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/meetup/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ab8c04b50fb34b9611bdab03d036677a",
+    "url": "/meetup/index.html"
+  }
+];
