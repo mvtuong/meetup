569e62d5f4adf7886accad508df5b932bc586ed8
mvtuong <mvtuong90@gmail.com>
2019-04-23 08:43:25 +0200

    Unit testing part a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 class App extends Component {
+  state = {
+    events: [],
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CitySearch />
+        <EventList events={this.state.events} />
       </div>
     );
   }
