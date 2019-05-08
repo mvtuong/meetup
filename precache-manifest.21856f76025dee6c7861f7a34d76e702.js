d718d4b382954f118e5cb7505d9d62701800db63
mvtuong <mvtuong90@gmail.com>
2019-05-08 16:31:59 +0200

    Updates

---
+++ precache-manifest.21856f76025dee6c7861f7a34d76e702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "630884e63bdb41a6077b",
+    "url": "/meetup/static/css/main.bd342648.chunk.css"
+  },
+  {
+    "revision": "630884e63bdb41a6077b",
+    "url": "/meetup/static/js/main.4b169dd0.chunk.js"
+  },
+  {
+    "revision": "448b25596e3b06413538",
+    "url": "/meetup/static/js/runtime~main.34b77dee.js"
+  },
+  {
+    "revision": "dd995083c3064e0b191b",
+    "url": "/meetup/static/js/2.78844fee.chunk.js"
+  },
+  {
+    "revision": "6a027d6e014003f248826e67527c6d1c",
+    "url": "/meetup/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.44f14eb3086e87459c459f925ed89844.js"
+  "/meetup/precache-manifest.21856f76025dee6c7861f7a34d76e702.js"
 );
 
 workbox.clientsClaim();
