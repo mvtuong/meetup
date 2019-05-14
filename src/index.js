749a4292b54d8ddcb9722a4606bbf9333131d15a
mvtuong <mvtuong90@gmail.com>
2019-05-14 10:38:42 +0200

    Set up Atatus monitoring

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('dbd1fde07f2b4ddab4b25cd7f02a6c0c').install();
