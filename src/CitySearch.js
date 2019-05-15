06a7ee4e0422f26fc4c2d453889969bb6a7aa7c0
mvtuong <mvtuong90@gmail.com>
2019-05-16 00:20:08 +0200

    OOP part a

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -12,6 +13,16 @@ class CitySearch extends Component {
     this.setState({ query: value });
     getSuggestions(value).then(suggestions => {
       this.setState({ suggestions });
+
+      if (value && suggestions.length === 0) {
+        this.setState({
+          infoText: 'We can not find the city you are looking for. Please try another city',
+        });
+      } else {
+        this.setState({
+          infoText: '',
+        });
+      }
     });
   }
 
@@ -23,6 +34,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
