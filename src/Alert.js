9201632b89e758ee1b061611ebad210d8697c124
mvtuong <mvtuong90@gmail.com>
2019-05-16 00:24:35 +0200

    OOP part b

--- src/Alert.js
+++ src/Alert.js
@@ -28,4 +28,18 @@ class InfoAlert extends Alert {
   }
 }
 
-export { InfoAlert };
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'red';
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      fontStyle: 'italic',
+    };
+  }
+}
+
+export { InfoAlert, ErrorAlert };
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
@@ -9,11 +10,17 @@ class NumberOfEvents extends Component {
     const value = event.target.value;
     this.setState({ number: value });
     this.props.updateEvents(null, null, value);
+    if (value < 1) {
+      this.setState({ errorText: 'Number should be at least 1' });
+    } else {
+      this.setState({ errorText: '' });
+    }
   }
 
   render() {
     return (
       <div className="NumberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
         <span>Show </span>
         <input
           type="number"
