dbc20055ce87a4eefe803cb29468bbcf29c3ff5c
mvtuong <mvtuong90@gmail.com>
2019-05-16 18:26:40 +0200

    PWA

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -4,10 +4,7 @@ describe('show/hide an event details', () => {
   let browser;
   let page;
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 250,
-    });
+    browser = await puppeteer.launch();
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.Event');
--- src/api.js
+++ src/api.js
@@ -93,6 +93,11 @@ async function getEvents(lat, lon, page) {
   if (window.location.href.startsWith('http://localhost')) {
     return mockEvents.events;
   }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    return JSON.parse(events);
+  }
   
   const token = await getAccessToken();
   if (token) {
@@ -108,7 +113,12 @@ async function getEvents(lat, lon, page) {
     }
 
     const result = await axios.get(url);
-    return result.data.events;
+    const events = result.data.events;
+    if (events.length) { // Check if the events are existed before storing
+      localStorage.setItem('lastEvents', JSON.stringify(events));
+    }
+
+    return events;
   }
   return [];
 }
