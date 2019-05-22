61d28b5e395d46c5b2e039f210f4e820a3ad25dc
mvtuong <mvtuong90@gmail.com>
2019-05-22 11:38:49 +0200

    Updates

--- precache-manifest.558730154e5d3414d6d640608443dd1f.js
+++ precache-manifest.558730154e5d3414d6d640608443dd1f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "29f023186832989b9252",
+    "revision": "0f97ad03cc29c9be6511",
     "url": "/meetup/static/css/main.b4916468.chunk.css"
   },
   {
-    "revision": "29f023186832989b9252",
-    "url": "/meetup/static/js/main.7facae66.chunk.js"
+    "revision": "0f97ad03cc29c9be6511",
+    "url": "/meetup/static/js/main.8ab5ab61.chunk.js"
   },
   {
     "revision": "448b25596e3b06413538",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/meetup/static/js/2.f29d7b3e.chunk.js"
   },
   {
-    "revision": "65191120f4864eb311d2bf103397f96d",
+    "revision": "818702779fa65176290c4222ccb608f1",
     "url": "/meetup/index.html"
   }
 ];
